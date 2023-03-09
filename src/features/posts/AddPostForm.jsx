1446b6351e5ac4aefd1eaf5d6333a8bc9ba0aa8d
Emmanuel Narcisse <epokweb@hotmail.fr>
2023-03-09 15:39:16 +0400

    Add author logic in state

--- src/features/posts/AddPostForm.jsx
+++ src/features/posts/AddPostForm.jsx
@@ -1,31 +1,37 @@
 import React, { useState } from 'react'
-import { useDispatch } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 import { postAdded } from './postsSlice'
-import { nanoid } from '@reduxjs/toolkit'
 
 export const AddPostForm = () => {
   const [title, setTitle] = useState('')
   const [content, setContent] = useState('')
+  const [userId, setUserId] = useState('')
+
+  const users = useSelector((state) => {
+    return state.users
+  })
 
   const dispatch = useDispatch()
 
   const onTitleChanged = (e) => setTitle(e.target.value)
   const onContentChanged = (e) => setContent(e.target.value)
+  const onUserChanged = (e) => setUserId(e.target.value)
 
   const handleSubmit = (e) => {
     if (title && content) {
-      dispatch(
-        postAdded({
-          id: nanoid(),
-          title: title,
-          content: content,
-        })
-      )
-
+      dispatch(postAdded(title, content, userId))
       setTitle('')
       setContent('')
+      setUserId('')
     }
   }
+
+  const canSave = Boolean(title) && Boolean(content) && Boolean(userId)
+  const usersSelect = users.map((user) => (
+    <option value={user.id} key={user.id}>
+      {user.name}
+    </option>
+  ))
 
   return (
     <section>
@@ -46,7 +52,12 @@ export const AddPostForm = () => {
           value={content}
           onChange={onContentChanged}
         />
-        <button type="button" onClick={handleSubmit}>
+        <label htmlFor="select-user">User:</label>
+        <select id="select-user" value={userId} onChange={onUserChanged}>
+          <option value="">--Select an user</option>
+          {usersSelect}
+        </select>
+        <button type="button" onClick={handleSubmit} disabled={!canSave}>
           Save Post
         </button>
       </form>
--- src/features/posts/SinglePostPage.jsx
+++ src/features/posts/SinglePostPage.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { useSelector } from 'react-redux'
+import { PostAuthor } from './PostAuthor'
 
 export const SinglePostPage = ({ match }) => {
   const { postId } = match.params
@@ -19,6 +20,7 @@ export const SinglePostPage = ({ match }) => {
     <section>
       <article className="post">
         <h2>{post.title}</h2>
+        <PostAuthor userId={post.user} />
         <p className="post-content">{post.content}</p>
       </article>
     </section>
