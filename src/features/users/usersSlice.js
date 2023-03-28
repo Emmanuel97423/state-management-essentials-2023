036821e63525c2354bddb440028a491c3a05c964
Emmanuel Narcisse <epokweb@hotmail.fr>
2023-03-28 10:15:30 +0400

    Feature: notification counter not read

--- src/features/users/usersSlice.js
+++ src/features/users/usersSlice.js
@@ -20,5 +20,6 @@ const usersSlice = createSlice({
 })
 
 export default usersSlice.reducer
-export const selectAllUsers = (state)=> state.users
-export const selectUserById =  (state, userId )=> state.users.find(user => user.id === userId ) 
+export const selectAllUsers = (state) => state.users
+export const selectUserById = (state, userId) =>
+  state.users.find((user) => user.id === userId)
