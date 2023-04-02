6be6b5135e642d8e47a6cf8c0d48733af125aa45
Emmanuel Narcisse <epokweb@hotmail.fr>
2023-04-02 22:35:03 +0400

    init: redux RTK

--- src/features/posts/SinglePostPage.jsx
+++ src/features/posts/SinglePostPage.jsx
@@ -1,27 +1,54 @@
 import React from 'react'
-import { useSelector } from 'react-redux'
+import { Link } from 'react-router-dom'
 import { PostAuthor } from './PostAuthor'
-import { selectPostById } from './postsSlice'
+import { Spinner } from '../../components/Spinner'
+import { useGetPostQuery } from '../api/apiSlice'
+import { TimeAgo } from './TimeAgo'
+import { ReactionButtons } from './ReactionButtons'
 
 export const SinglePostPage = ({ match }) => {
   const { postId } = match.params
 
-  const post = useSelector((state) => selectPostById(state, postId))
+  // const post = useSelector((state) => selectPostById(state, postId))
+  const { data: post, isFetching, isSuccess } = useGetPostQuery(postId)
 
-  if (!post) {
-    return (
-      <section>
-        <h2>Post not found</h2>
-      </section>
-    )
-  }
-  return (
-    <section>
+  let content
+
+  if (isFetching) {
+    content = <Spinner text="Loading..." />
+  } else if (isSuccess) {
+    content = (
       <article className="post">
         <h2>{post.title}</h2>
-        <PostAuthor userId={post.user} />
+        <div>
+          <PostAuthor userId={post.user} />
+          <TimeAgo timestamp={post.date} />
+        </div>
         <p className="post-content">{post.content}</p>
+        <ReactionButtons post={post} />
+        <Link to={`/editPost/${post.id}`} className="button">
+          Edit post
+        </Link>
       </article>
-    </section>
-  )
+    )
+  }
+
+  return <section>{content}</section>
+
+  // if (!post) {
+  //   return (
+  //     <section>
+  //       <h2>Post not found</h2>
+  //     </section>
+  //   )
+  // }
+  // return (
+  //   <section>
+  //     <article className="post">
+  //       <h2>{post.title}</h2>
+  //       <PostAuthor userId={post.user} />
+  //       <p className="post-content">{post.content}</p>
+  //     </article>
+  //   </section>
+  // )
 }
